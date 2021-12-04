0eb35c98e331cab9fb77e81e0814630bff528cee
vivianc2 <86415736+vivianc2@users.noreply.github.com>
2021-12-03 21:51:56 -0500

    create branch and rebase (#1)
    Co-authored-by: Vivian Chen <vivianchen@dhcp-vl2041-180.eduroam.cornell.edu>

--- printer.py
+++ printer.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def fun1():
-    a = np.array([[1,2,3],[4,5,6]])
+    a = np.array([[13256457,6542,3],[4,5,6]])
     print(a)
 
 fun1()
 
-# added some comments when probably I'm on main
+print("SomethingFun")
